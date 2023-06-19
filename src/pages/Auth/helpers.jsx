ee957f41654924d137a13362b64c572b15616a4a
Juan Castillo <Juanpablodlcc@gmail.com>
2023-06-19 15:27:48 -0600

    [FIX] auth

--- src/pages/Auth/helpers.jsx
+++ src/pages/Auth/helpers.jsx
@@ -1,7 +1,7 @@
 import * as Yup from 'yup';
 
 export const Schema = Yup.object().shape({
-    email: Yup.string().email("Formato invalido").required('Campo Obligatorio'),
+    email: Yup.string().email("Formato invalido, se espera un email valido").required('Campo Obligatorio'),
     password: Yup.string().required('Campo Obligatorio'),
 
 });
--- src/pages/Auth/login.jsx
+++ src/pages/Auth/login.jsx
@@ -36,7 +36,6 @@ function Login() {
                                         }
                                     )
                                 } catch (err) {
-
                                     console.log(err)
                                 }
                             }}
