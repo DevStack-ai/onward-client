b6997fe95d6941c6490210758ced25602c4f1f17
Juan Castillo <juanpablodlcc@gmail.com>
2024-02-23 13:50:18 -0600

    [ADD] favicon

--- src/pages/Tracker/_helpers.jsx
+++ src/pages/Tracker/_helpers.jsx
@@ -70,147 +70,188 @@ export const Columns = [{
     accesor: "reference",
     className: "ref",
     headerStyle: { position: "sticky", left: 0, backgroundColor: "#EAEAEA" },
+    cell: (container) => <td className="text-center">{container.value}</td>,
+
 },
 {
     title: "REF2",
     accesor: "reference_alt",
+    cell: (container) => <td className="text-center">{container.value}</td>,
 },
 {
     title: "DOC NO",
     accesor: "docto_no",
+    cell: (container) => <td style={{ textAlign: "right" }}>{container.value}</td>,
+
 },
 {
     title: "SOURCE",
     accesor: "source",
+    cell: (container) => <td className="text-center">{container.value}</td>,
+
 },
 {
     title: "COMPANY",
     accesor: "company",
+    cell: (container) => <td className="text-center">{container.value}</td>,
+
 },
 
 {
     title: "CUSTOMER",
     accesor: "customer",
-    className: "large"
+    className: "large text-center",
+
 },
 {
     title: "SOURCE/COUNTRY",
     accesor: "country",
+    cell: (container) => <td className="large text-center">{container.value}</td>,
+
 },
 {
     title: "Status BPO",
     accesor: "status_bpo",
-    cell: (container) => <td className="large">{container.bpo_livemapurl ? <a href={container.bpo_livemapurl} target='_blank'>{container.status_bpo}</a> : container.status_bpo}</td>,
+    cell: (container) => <td className="large text-center">{container.bpo_livemapurl ? <a href={container.bpo_livemapurl} target='_blank'>{container.status_bpo}</a> : container.status_bpo}</td>,
     className: "large"
 },
 {
     title: "EXTERNAL STATUS",
     accesor: "status",
-    cell: (container) => <td className="large">{container.LiveMapUrl ? <a href={container.LiveMapUrl} target='_blank'>{container.status}</a> : container.status}</td>,
+    cell: (container) => <td className="large  text-center">{container.LiveMapUrl ? <a href={container.LiveMapUrl} target='_blank'>{container.status}</a> : container.status}</td>,
     className: "large"
 },
 {
     title: "SALES DATE",
-    cell: (container) => <td className="large">{container.value ? moment(container.value).format("MMMDDYYYY").toUpperCase() : "-"}</td>,
+    cell: (container) => <td className="large  text-center">{container.value ? moment(container.value).format("MMMDDYYYY").toUpperCase() : "-"}</td>,
     accesor: "close_date",
 },
 {
     title: "WH DEPARTURE DATE",
     accesor: "checkout_date",
-    cell: (container) => <td className="large">{container.value ? moment(container.value).format("MMMDDYYYY").toUpperCase() : "-"}</td>,
+    cell: (container) => <td className="large text-center" >{container.value ? moment(container.value).format("MMMDDYYYY").toUpperCase() : "-"}</td>,
 
 },
 {
     title: "DEPARTURE DATE",
     accesor: "departure_data",
-    cell: (container) => <td className="large">{container.value ? moment(container.value).format("MMMDDYYYY").toUpperCase() : "-"}</td>,
+    cell: (container) => <td className="large text-center">{container.value ? moment(container.value).format("MMMDDYYYY").toUpperCase() : "-"}</td>,
 
 },
 {
     title: "ARRIVAL DATE",
     accesor: "arrival_date",
-    cell: (container) => <td className="large">{container.value ? moment(container.value).format("MMMDDYYYY").toUpperCase() : "-"}</td>,
+    cell: (container) => <td className="large  text-center">{container.value ? moment(container.value).format("MMMDDYYYY").toUpperCase() : "-"}</td>,
 
 },
 {
     title: "Entry Number",
     accesor: "entry_number",
-    className: "large"
+    className: "large  text-center"
 },
 {
     title: "INVOICE AMOUNT",
     accesor: "total_amount",
     className: "large",
-    cell: (container) => <>{container.value ? `$${numberWithCommas(container.value)}` : ""}</>,
+    cell: (container) => <td className="large" style={{ textAlign: "right" }}>{container.value ? `$${numberWithCommas(container.value)}` : ""}</td>,
 
 },
 {
     title: "FDA",
-    cell: (container) => <td className={`large ${isOK(container.value)}`}>{container.value}</td>,
+    cell: (container) => <td className={`large text-center ${isOK(container.value)}`}>{container.value}</td>,
     accesor: "fda",
 },
 {
     title: "CBP",
-    cell: (container) => <td className={`large ${isOK(container.value)}`}>{container.value}</td>,
+    cell: (container) => <td className={`large text-center ${isOK(container.value)}`}>{container.value}</td>,
     accesor: "cbp",
 },
 {
     title: "USDA",
-    cell: (container) => <td className={`large ${isOK(container.value)}`}>{container.value}</td>,
+    cell: (container) => <td className={`large text-center ${isOK(container.value)}`}>{container.value}</td>,
     accesor: "usda",
 },
 {
     title: "LFD",
     accesor: "lfd",
-    cell: (container) => <td className="large">{container.value ? moment(container.value).format("MMMDDYYYY").toUpperCase() : "-"}</td>,
+    cell: (container) => <td className="large text-center">{container.value ? moment(container.value).format("MMMDDYYYY").toUpperCase() : "-"}</td>,
 
 },
 {
     title: "LFD Fee",
     accesor: "lfd_fee",
+    cell: (container) => <td style={{ textAlign: "right" }}>{container.value}</td>,
 },
 {
     title: "EST DELIVERY DATE",
     accesor: "estimated_date",
-    cell: (container) => <td className="large">{container.value ? moment(container.value).format("MMMDDYYYY").toUpperCase() : "-"}</td>,
+    cell: (container) => <td className="large text-center">{container.value ? moment(container.value).format("MMMDDYYYY").toUpperCase() : "-"}</td>,
 
     className: "large"
 },
 {
     title: "DELIVERY DATE",
     accesor: "delivery_date",
-    cell: (container) => <td className="large">{container.value ? moment(container.value).format("MMMDDYYYY").toUpperCase() : "-"}</td>,
-
+    cell: (container) => <td className="large text-center">{container.value ? moment(container.value).format("MMMDDYYYY").toUpperCase() : "-"}</td>,
     className: "large"
 },
 {
     title: "OBS",
     accesor: "obs",
-    className: "large"
+    className: "large text-center"
 
 },
-{ title: "CONTAINER #", accesor:"ContainerNumber" },
-{ title: "Message", },
-{ title: "StatusId", },
-{ title: "ReferenceNo", className: "large" },
-{ title: "ShippingLine", },
-{ title: "FromCountry", },
-{ title: "Pol", },
-{ title: "Pod", },
-{ title: "Vessel", },
+{
+    title: "CONTAINER # text-center",
+    accesor: "ContainerNumber", cell: (container) => <td className="large">{container.value}</td>,
+},
+{
+    title: "Message",
+    className: "large text-center"
+},
+{
+    title: "StatusId",
+    className: "large text-center"
+},
+{
+    title: "ReferenceNo",
+    className: "large text-center"
+},
+{
+    title: "ShippingLine",
+    className: "large text-center"
+},
+{
+    title: "FromCountry",
+    className: "large text-center"
+},
+{
+    title: "Pol",
+    className: "large text-center"
+},
+{
+    title: "Pod",
+    className: "large text-center"
+},
+{
+    title: "Vessel",
+    className: "large text-center"
+},
 {
     title: "VesselIMO",
+    className: "large text-center"
 
 },
 {
     title: "GateOutDate",
-    cell: (container) => <td className="large">{container.value ? moment(container.value).format("MMMDDYYYY").toUpperCase() : "-"}</td>,
+    cell: (container) => <td className="large text-center">{container.value ? moment(container.value).format("MMMDDYYYY").toUpperCase() : "-"}</td>,
 },
-{ title: "FormatedTransitTime", },
+{ title: "FormatedTransitTime",    className: "large text-center"
+},
 {
     title: "Ultima actualizacion", accesor: "last_api_request",
 
-    cell: (container) => <td className="large">{container.value ? moment(container.value).subtract(6, "hours").format("MMMDDYYYY HH:mm:ss").toUpperCase() : "-"}</td>,
+    cell: (container) => <td className="large text-center">{container.value ? moment(container.value).subtract(6, "hours").format("MMMDDYYYY HH:mm:ss").toUpperCase() : "-"}</td>,
 
 },
 
